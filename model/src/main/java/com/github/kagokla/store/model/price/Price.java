add76fead6031a267d75de2d8229aa111207faf5
kagokla <kagokla@extremenetworks.com>
2024-04-04 18:12:43 +0200

    Simplify Price implementation

--- model/src/main/java/com/github/kagokla/store/model/price/Price.java
+++ model/src/main/java/com/github/kagokla/store/model/price/Price.java
@@ -1,36 +1,34 @@
 package com.github.kagokla.store.model.price;
 
+import com.github.kagokla.store.model.utils.ValidatorUtils;
+
 import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.Objects;
 
 public record Price(BigDecimal amount, Currency currency) {
 
     public Price {
-        Objects.requireNonNull(amount, "amount must not be null");
-        if (amount.signum() < 0) {
-            throw new IllegalArgumentException("amount must not be negative");
-        }
+        ValidatorUtils.requireNonNull(amount, "amount must not be null");
+        ValidatorUtils.requireNonNegative(amount.signum(), "amount must not be negative");
+        ValidatorUtils.requireNonNull(currency, "currency must not be null");
 
-        Objects.requireNonNull(currency, "currency must not be null");
         if (amount.scale() > currency.getDefaultFractionDigits()) {
             throw new IllegalArgumentException("Scale of amount %s is greater than the scale of the currency %s".formatted(amount, currency));
         }
     }
 
     public Price add(final Price other) {
-        Objects.requireNonNull(other, "other must not be null");
-
+        ValidatorUtils.requireNonNull(other, "other must not be null");
         if (!currency.equals(other.currency())) {
-            throw new UnsupportedOperationException("Addition must be performed with same currencies");
+            throw new IllegalArgumentException("Addition must be performed with same currencies");
         }
+
         return new Price(amount.add(other.amount), currency);
     }
 
     public Price multiply(final int quantity) {
-        if (quantity < 0) {
-            throw new UnsupportedOperationException("Multiplication must be performed with a positive quantity");
-        }
+        ValidatorUtils.requireNonNegative(quantity, "quantity must not be negative");
+
         return new Price(amount.multiply(BigDecimal.valueOf(quantity)), currency);
     }
 }
