58dc264b80e1db50bf6c139b32fe0bdcd6fca900
kagokla <kagokla@extremenetworks.com>
2024-04-04 18:11:13 +0200

    Use lombok to hide the private constructor

--- model/src/main/java/com/github/kagokla/store/model/utils/IdGeneratorUtils.java
+++ model/src/main/java/com/github/kagokla/store/model/utils/IdGeneratorUtils.java
@@ -1,16 +1,17 @@
 package com.github.kagokla.store.model.utils;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.security.SecureRandom;
 import java.util.Base64;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class IdGeneratorUtils {
     public static final String PRODUCT_ID_PREFIX = "prd";
     public static final String CUSTOMER_ID_PREFIX = "cst";
     private static final SecureRandom random = new SecureRandom();
     private static final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
-
-    private IdGeneratorUtils() {
-    }
 
     public static String generateRandomId() {
         final var buffer = new byte[20];
