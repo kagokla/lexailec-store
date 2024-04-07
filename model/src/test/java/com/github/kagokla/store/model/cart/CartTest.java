1db5bb595b88b30fa03f49df01913aa310f1b456
kagokla <kagokla@extremenetworks.com>
2024-04-07 23:29:33 +0200

    Improve the tests related to Cart

--- model/src/test/java/com/github/kagokla/store/model/cart/CartTest.java
+++ model/src/test/java/com/github/kagokla/store/model/cart/CartTest.java
@@ -1,20 +1,24 @@
 package com.github.kagokla.store.model.cart;
 
+import com.github.kagokla.store.model.ModelTestBase;
 import com.github.kagokla.store.model.utils.IdGeneratorUtils;
 import org.junit.jupiter.api.Test;
+
+import javax.money.Monetary;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-class CartTest {
-
-    private Cart cart;
+class CartTest extends ModelTestBase {
 
     @Test
     void shouldCreateNewCart() {
-        final var cart = new Cart(IdGeneratorUtils.generateRandomCustomerId());
+        final var cart = buildRandomCart();
 
         assertThat(cart).isNotNull();
+        assertThat(cart.getId()).startsWith(IdGeneratorUtils.CART_ID_PREFIX);
+        assertThat(cart.getCurrency()).isEqualByComparingTo(Monetary.getCurrency("EUR"));
     }
 
     @Test
@@ -25,5 +29,27 @@ class CartTest {
     @Test
     void shouldFailWhenCustomerIdIsEmpty() {
         assertThatIllegalArgumentException().isThrownBy(() -> new Cart(""));
+    }
+
+    @Test
+    void shouldSucceedWhenAddingItemToCart() throws NotEnoughItemsInStockException, MaxCartItemsReachedException {
+        final var cart = buildRandomCart();
+
+        cart.addLineItemOrIncreaseLineItem(buildRandomProduct(), 5);
+        cart.addLineItemOrIncreaseLineItem(buildRandomProduct(), 2);
+
+        assertThat(cart).isNotNull();
+        assertThat(cart.getLineItems()).isEqualTo(List.of(5, 2));
+    }
+
+    @Test
+    void shouldReturnStringRepresentation() {
+        final var cart = buildRandomCart();
+
+        assertThat(cart).isNotNull().asString().startsWith("Cart");
+    }
+
+    private Cart buildRandomCart() {
+        return new Cart(IdGeneratorUtils.generateRandomCustomerId());
     }
 }
