199e03a634f56276d54249688ae54efea29b067d
kagokla <kagokla@extremenetworks.com>
2024-04-07 23:30:48 +0200

    Implement the class Product

---
+++ model/src/main/java/com/github/kagokla/store/model/product/Product.java
@@ -0,0 +1,46 @@
+package com.github.kagokla.store.model.product;
+
+import com.github.kagokla.store.model.BaseEntity;
+import com.github.kagokla.store.model.price.Price;
+import com.github.kagokla.store.model.utils.IdGeneratorUtils;
+import com.github.kagokla.store.model.utils.ValidatorUtils;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Product extends BaseEntity {
+
+    private String name;
+    @Setter
+    private String description;
+    private Price price;
+    private int stock;
+
+    public Product(final String name, final String description, final Price price, final int stock) {
+        super(IdGeneratorUtils.generateRandomProductId());
+
+        setName(name);
+        this.description = description;
+        setPrice(price);
+        setStock(stock);
+    }
+
+    public void setName(final String name) {
+        ValidatorUtils.requireNonBlank(name, "name");
+        this.name = name;
+    }
+
+    public void setPrice(final Price price) {
+        ValidatorUtils.requireNonNull(price, "price");
+        this.price = price;
+    }
+
+    public void setStock(final int stock) {
+        ValidatorUtils.requireNonNegative(stock, "stock");
+        this.stock = stock;
+    }
+
+    public String toString() {
+        return "Product(Id=" + id + ", name=" + name + ", price=" + price + ")";
+    }
+}
