d519fb4237a4a63223aeca32a9ee7c2bc0c2aeab
kagokla <kagokla@extremenetworks.com>
2024-04-07 23:28:57 +0200

    Beautify the code of ValidatorUtils

--- model/src/main/java/com/github/kagokla/store/model/utils/ValidatorUtils.java
+++ model/src/main/java/com/github/kagokla/store/model/utils/ValidatorUtils.java
@@ -7,18 +7,21 @@ import org.apache.commons.lang3.StringUtils;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ValidatorUtils {
 
-    public static <T> void requireNonNull(T propertyValue, String propertyName) {
-        if (null == propertyValue)
+    public static <T> void requireNonNull(final T propertyValue, final String propertyName) {
+        if (null == propertyValue) {
             throw new IllegalArgumentException("%s must not be null".formatted(propertyName));
+        }
     }
 
-    public static void requireNonBlank(final String propertyValue, String propertyName) {
-        if (StringUtils.isBlank(propertyValue))
+    public static void requireNonBlank(final String propertyValue, final String propertyName) {
+        if (StringUtils.isBlank(propertyValue)) {
             throw new IllegalArgumentException("%s must not be blank".formatted(propertyName));
+        }
     }
 
-    public static void requireNonNegative(final int propertyValue, String propertyName) {
-        if (propertyValue < 0)
+    public static void requireNonNegative(final int propertyValue, final String propertyName) {
+        if (propertyValue < 0) {
             throw new IllegalArgumentException("%s must not be negative".formatted(propertyName));
+        }
     }
 }
